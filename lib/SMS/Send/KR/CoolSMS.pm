e959adb6f50e4ef236aaa7e981629b64ff91b084
Keedi Kim <keedi.k@gmail.com>
2013-02-23 03:00:46 +0900

    Add document first to design module

--- lib/SMS/Send/KR/CoolSMS.pm
+++ lib/SMS/Send/KR/CoolSMS.pm
@@ -9,23 +9,29 @@ __END__
 
 =head1 SYNOPSIS
 
-    use SMS::Send::KR::CoolSMS
+    use SMS::Send;
 
     # create the sender object
     my $sender = SMS::Send->new('KR::CoolSMS',
+        _ssl      => 1,
         _user     => 'keedi',
         _password => 'mypass',
+        _type     => 'sms',
+        _from     => '01025116893',
     );
+
     # send a message
     my $sent = $sender->send_sms(
-        text => 'You message may use up to 80 chars',
-        to'  => '+49 555 4444',
+        text => 'You message may use up to 80 chars and must be utf8',
+        to   => '01025116893',
     );
-    
-    if ( $sent ) {
-        print "Sent message\n";
-    } else {
-        print "Failed to send test message\n";
+
+    unless ( $sent->{success} ) {
+        warn "failed to send sms: $sent->{reason}\n";
+
+        # if you want to know detail more, check $sent->{detail}
+        use Data::Dumper;
+        warn Dumper $sent->{detail};
     }
 
 
